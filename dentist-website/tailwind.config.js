f4e006d4e7ce5aac1667290bdec71976078c094f
Wajktor13 <wiktorwilkusz13@gmail.com>
2023-09-03 14:15:44 +0200

    Add navigation

--- dentist-website/tailwind.config.js
+++ dentist-website/tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': {'max': '640px'},
+      'md': {'max': '800px'},
+      'lg': {'max': '1024px'},
+      'xl': {'max': '1280px'},
+      '2xl': {'max': '1536px'},
+    }
   },
   plugins: [],
 }
