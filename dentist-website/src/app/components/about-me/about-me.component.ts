982be97fb6e10fe7116836afa48a3521c6c16a2d
Wajktor13 <wiktorwilkusz13@gmail.com>
2023-09-03 20:06:03 +0200

    Add 'about me' section background

---
+++ dentist-website/src/app/components/about-me/about-me.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
+
+
+@Component({
+    selector: 'app-about-me',
+    templateUrl: './about-me.component.html',
+    styleUrls: ['./about-me.component.css']
+})
+
+export class AboutMeComponent implements OnInit, OnDestroy {
+    public imagesUrls: string[] = ["assets/clinic/1.jpg", "assets/clinic/2.jpg", "assets/clinic/3.jpg", "assets/clinic/4.jpg", "assets/clinic/5.jpg"];
+    public currentImageIndex: number = 0;
+    private intervalTime: number = 4000;
+    private changeBackgroundSub: Subscription | null = null;
+
+    public ngOnInit(): void {
+        this.loadImages();
+
+        this.changeBackgroundSub = interval(this.intervalTime).subscribe(_ => this.currentImageIndex = (this.currentImageIndex + 1) % this.imagesUrls.length);
+    }
+
+    public ngOnDestroy(): void {
+        this.changeBackgroundSub?.unsubscribe();
+    }
+
+    public getCurrentImageUrl(): string {
+        return this.imagesUrls[this.currentImageIndex];
+    }
+
+    private loadImages(): void{
+        for(let i=0; i < this.imagesUrls.length; i++){
+            let img = new Image();
+            img.src = this.imagesUrls[i];
+            console.log('laoded')
+        }
+    }
+}
