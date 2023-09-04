e700e8cf1765a498dfe96e3bbb3136f1b6c3124e
Wajktor13 <wiktorwilkusz13@gmail.com>
2023-09-04 17:27:22 +0200

    Add certificates component

---
+++ dentist-website/src/app/components/certificates-section/certificates-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certificates-section',
+  templateUrl: './certificates-section.component.html',
+  styleUrls: ['./certificates-section.component.css']
+})
+export class CertificatesSectionComponent {
+
+}
