8c8d6d93b156876dceec2ea0310908c51bc62ac5
Wajktor13 <wiktorwilkusz13@gmail.com>
2023-09-07 12:51:17 +0200

    Add fixed nav to big screen view

--- dentist-website/src/app/components/navigation/navigation.component.ts
+++ dentist-website/src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 
 @Component({
@@ -7,10 +7,32 @@ import { Component } from '@angular/core';
     styleUrls: ['./navigation.component.css']
 })
 
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
     public showMobileMenu: boolean = false;
+    private breakpoint: number = 108; 
+    private menuFixed: boolean = false;
+    private menu: HTMLDivElement | null = null;
+
+    public ngOnInit(): void {
+        this.menu = document.getElementById("menu") as HTMLDivElement;
+    }
 
     public toggleMobileMenu(): void{
-        this.showMobileMenu = !this.showMobileMenu
+        this.showMobileMenu = !this.showMobileMenu;
+    }
+
+    @HostListener("window:scroll") 
+    public onWindowScroll() {
+        
+        if (window.scrollY >= this.breakpoint && !this.menuFixed && this.menu != null) {
+            this.menuFixed = true;
+            this.menu.style.position = "fixed";
+            this.menu.style.top = "35px";
+
+        } else if (window.scrollY < this.breakpoint && this.menuFixed && this.menu != null) {
+            this.menuFixed = false;
+            this.menu.style.position = "relative";
+            this.menu.style.top = "0";
+        }
     }
 }
