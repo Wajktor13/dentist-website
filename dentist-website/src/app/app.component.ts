adfd919da69fb31e074b31697aa091fdae953cf3
Wajktor13 <wiktorwilkusz13@gmail.com>
2023-09-08 14:30:31 +0200

    Add AOS

--- dentist-website/src/app/app.component.ts
+++ dentist-website/src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component, AfterViewInit } from '@angular/core';
+import * as AOS from "aos";
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 
 export class AppComponent implements AfterViewInit {
-  title = 'dentist-website';
-  
-  public ngAfterViewInit() {
-  }
+    title = 'dentist-website';
+    
+    public ngAfterViewInit() {
+        AOS.init();
+    }
 }
