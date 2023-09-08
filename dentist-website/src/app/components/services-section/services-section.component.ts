37cc6657eab96da0988ffa626a232e2ffca4acfa
Wajktor13 <wiktorwilkusz13@gmail.com>
2023-09-08 08:29:54 +0200

    Add vanilla-tilt.js

--- dentist-website/src/app/components/services-section/services-section.component.ts
+++ dentist-website/src/app/components/services-section/services-section.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import VanillaTilt from "vanilla-tilt"
+
 
 @Component({
-  selector: 'app-services-section',
-  templateUrl: './services-section.component.html',
-  styleUrls: ['./services-section.component.css']
+    selector: 'app-services-section',
+    templateUrl: './services-section.component.html',
+    styleUrls: ['./services-section.component.css']
 })
-export class ServicesSectionComponent {
 
+export class ServicesSectionComponent implements OnInit {
+
+    constructor (
+        private elRef: ElementRef
+        ) { }
+
+    ngOnInit() {
+        VanillaTilt.init(
+            this.elRef.nativeElement.querySelectorAll(".service-tile"), 
+            {   max: 25,
+                speed: 400,
+                scale: 1.05,
+                glare: true
+            });
+        }
 }
