91b51ae9f709ba434a951ae7dc978316ad5cb1ed
Wajktor13 <wiktorwilkusz13@gmail.com>
2023-09-09 13:14:43 +0200

    Add basic gallery view

--- dentist-website/src/app/components/gallery/gallery.component.ts
+++ dentist-website/src/app/components/gallery/gallery.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-gallery',
-  templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.css']
-})
-export class GalleryComponent {
 
+@Component({
+    selector: 'app-gallery',
+    templateUrl: './gallery.component.html',
+    styleUrls: ['./gallery.component.css']
+})
+
+export class GalleryComponent {
+    private noImages: number = 15;
+    public numbers: number[];
+
+    constructor() {
+        this.numbers = Array(this.noImages).fill(0).map((_, i)=>i);
+    }
+
+    public galleryImageClicked(event: Event){
+        console.log((event.target as HTMLImageElement).src);
+    }
 }
