e43fd4de007c2b2a0eb9a887b8012f13e27e31bf
Wajktor13 <wiktorwilkusz13@gmail.com>
2023-09-11 13:08:09 +0200

    Add OpenLayers map

--- dentist-website/src/app/components/contact-section/contact-section.component.ts
+++ dentist-website/src/app/components/contact-section/contact-section.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Map, View } from 'ol';
+import { OSM } from 'ol/source';
+import TileLayer from 'ol/layer/Tile';
+import { fromLonLat } from 'ol/proj';
+
 
 @Component({
   selector: 'app-contact-section',
   templateUrl: './contact-section.component.html',
-  styleUrls: ['./contact-section.component.css']
+  styleUrls: ['./contact-section.component.css'],
 })
-export class ContactSectionComponent {
 
-}
+export class ContactSectionComponent implements OnInit {
+    private map!: Map;
+  
+    public ngOnInit(): void {
+        this.configureMap();
+    }
+
+    private configureMap() {
+        this.map = new Map({
+            layers: [
+              new TileLayer({
+                source: new OSM(),
+              }),
+            ],
+            target: 'map',
+            view: new View({
+              center: fromLonLat([21.77247, 49.68803]),
+              zoom: 18,
+            }),
+          });
+    }
+  }
